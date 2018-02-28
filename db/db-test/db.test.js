44725eb276ac39cb5a13156b3bf73184cf374347
Mat Bagnall <matbagnall@gmail.com>
2018-02-27 16:43:39 -0800

    added fake data file and edited tests to accomodate

--- db/db-test/db.test.js
+++ db/db-test/db.test.js
@@ -1,95 +1,41 @@
 const dbHelpers = require('../dbHelpers');
 const mongoose = require('mongoose');
-
-
-const url = 'mongodb://localhost/testDB';
-
-const testSchema = mongoose.Schema({
-  id: { type: Number, index: { unique: true } },
-  name: String,
-  menu: {
-    lunch: [{
-      foodItem: String,
-      cost: Number,
-      tags: String,
-    }],
-    dinner: [{
-      foodItem: String,
-      cost: Number,
-      tags: String,
-    }],
-    dessert: [{
-      foodItem: String,
-      cost: Number,
-      tags: String,
-    }],
-  },
-});
-
-const TestColl = mongoose.model('specColl', testSchema);
-
-const testItem = new TestColl({
-  id: 9871,
-  name: 'Mat\' test resaturant',
-  menu: {
-    lunch: [{
-      foodItem: 'tasty bacon',
-      cost: 10,
-      tags: 'gluten-free',
-    }],
-    dinner: [{
-      foodItem: 'bacon soup',
-      cost: 11,
-      tags: 'vegan',
-    }],
-    dessert: [{
-      foodItem: 'maple bacon pancakes',
-      cost: 12,
-      tags: 'vegetarian',
-    }],
-  },
-});
+const seed = require('./fakeData');
 
 describe('Database Tests', () => {
-  let data = '';
-  // Before starting the test, create a sandboxed database connection
-  // Once a connection is established invoke done()
   beforeAll((done) => {
-    mongoose.connect(url);
+    mongoose.connect('mongodb://localhost/testDB');
     const db = mongoose.connection;
     db.on('error', console.error.bind(console, 'connection error'));
     db.once('open', () => {
-      console.log('We are connected to test database!', url);
-      done();
-    });
-  });
-
-  beforeEach((done) => {
-    TestColl.remove({}, () => {
-      console.log('collection dropped');
-      dbHelpers.save([testItem], TestColl, (result) => {
-        if (result) {
-          done();
-        }
+      console.log('We are connected to test database!');
+      seed.TestColl.remove({}, () => {
+        dbHelpers.save(seed.testData, seed.TestColl, (result) => {
+          if (result) {
+            console.log('====>', result);
+            done();
+          }
+        });
       });
     });
   });
 
   // Save object with 'name' value of 'Mike"
-  it('should save data to test database', async (done) => {
-    expect.assertions(1);
-    dbHelpers.find(9871, TestColl, (err, docs) => {
+  it('should save data the correct data to test database', async (done) => {
+    expect.assertions(2);
+    dbHelpers.find(90976, seed.TestColl, (err, docs) => {
       if (err) { throw err; }
-      data = docs;
       console.log('data ===> ', docs);
-      expect(data.length).toBe(1);
+      expect(docs.length).toBe(8);
+      expect(docs[0].id).toBe(90976);
       done();
     });
   });
 
   afterAll((done) => {
     mongoose.connection.db.dropDatabase(() => {
-      mongoose.connection.close(done);
+      mongoose.connection.close();
+      done();
     });
   });
 });
