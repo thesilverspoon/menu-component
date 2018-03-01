59cf16223e3c748c75c3b771c933fb67fa52ce5c
Mat Bagnall <matbagnall@gmail.com>
2018-02-28 19:50:02 -0800

    seeded database, refactored tests to check that it works correctly

--- db/db-test/db.test.js
+++ db/db-test/db.test.js
@@ -1,29 +1,12 @@
 const dbHelpers = require('../dbHelpers');
 const mongoose = require('mongoose');
-const seed = require('./testData');
 
 describe('database helpers - save and find', () => {
-  beforeAll((done) => {
-    mongoose.connect('mongodb://localhost/testDB');
-    const db = mongoose.connection;
-    db.on('error', console.error.bind(console, 'connection error'));
-    db.once('open', () => {
-      seed.TestColl.remove({}, () => {
-        dbHelpers.save({ data: seed.testData, model: seed.TestColl }, (result) => {
-          if (result) {
-            done();
-          }
-        });
-      });
-    });
-  });
-  // total items in sampleData is 8
   it('should save all data items to test database', (done) => {
-    expect.assertions(2);
-    dbHelpers.find({ query: '{}', model: seed.TestColl }, (err, docs) => {
+    expect.assertions(1);
+    dbHelpers.find({ query: '{}', model: dbHelpers.Restaurant }, (err, docs) => {
       if (err) { throw err; }
-      expect(docs.length).toBe(8);
-      expect(docs[0].id).toBe(90976);
+      expect(docs.length).toBe(119);
       done();
     });
   });
@@ -31,7 +14,7 @@ describe('database helpers - save and find', () => {
   // nb: the sample data contains a duplication of the restaurant with id: 89104
   it('should not save duplicate restaurants', async (done) => {
     expect.assertions(2);
-    dbHelpers.find({ id: 89104, model: seed.TestColl }, (err, docs) => {
+    dbHelpers.find({ id: 89104, model: dbHelpers.Restaurant }, (err, docs) => {
       if (err) { throw err; }
       expect(docs.length).toBe(1);
       expect(docs[0].id).toBe(89104);
@@ -41,7 +24,7 @@ describe('database helpers - save and find', () => {
 
   it('should return specified data fields only', (done) => {
     expect.assertions(4);
-    dbHelpers.find({ query: 'id menu.dinner', model: seed.TestColl }, (err, docs) => {
+    dbHelpers.find({ query: 'id menu.dinner', model: dbHelpers.Restaurant }, (err, docs) => {
       if (err) { throw err; }
       const docsObj = docs[0].toObject();
       expect(docs.length).toBe(1);
@@ -53,8 +36,6 @@ describe('database helpers - save and find', () => {
   });
 
   afterAll((done) => {
-    mongoose.connection.db.dropDatabase(() => {
-      mongoose.disconnect(done);
-    });
+    mongoose.disconnect(done);
   });
 });
